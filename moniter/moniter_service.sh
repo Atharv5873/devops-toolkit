5de4657f0a317293f8adae91eca2efa6e3669646
Atharv5873 <atharv5873@gmail.com>
2025-05-21 14:06:14 +0530

    Modified the script to check multiple scripts and log results

--- moniter/moniter_service.sh
+++ moniter/moniter_service.sh
@@ -1,37 +1,24 @@
 #!/bin/bash
 
-#Author:        Atharv Sharma
-#Date Created:  26/04/2025
-#Date Modified: 26/04/2025
+# Author:        Atharv Sharma
+# Date Created:  26/04/2025
+# Date Modified: 21/05/2025
 
-#Description:
-#This script checks if a given service is active and logs the status.
-
-#Usage:
-#Run this Script and select a servie from the menu
-
+# Description:
+# This script checks if a list of services are active and logs the status.
 
 LOG_DIR="/var/log/devops_toolkit"
 LOG_FILE="$LOG_DIR/service_status.log"
+SERVICES=("nginx" "mariadb" "docker" "ssh" "cron" "apache2")
 
 sudo mkdir -p "$LOG_DIR"
 
-SERVICES=("nginx" "mariadb" "docker" "ssh" "cron" "apache2")
-
-PS3="Select a service to check: "
-select SERVICE in "${SERVICES[@]}";do
-    if [[ -n "$SERVICE" ]];then
-        break
+for SERVICE in "${SERVICES[@]}"; do
+    if systemctl is-active --quiet "$SERVICE"; then
+        status="active"
     else
-        echo "Invalid Selection. Please Retry: "
+        status="inactive"
     fi
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Service '$SERVICE' is $status" | sudo tee -a "$LOG_FILE" > /dev/null
 done
-    
-if systemctl is-active --quiet "$SERVICE";then
-    status="active"
-else
-    status="inactive"
-fi
-
-echo "$(date '+%y-%m-%d %H:%M:%S') Service '$SERVICE' is $status" | sudo tee -a "$LOG_FILE" > /dev/null
-echo "Service '$SERVICE' status logged successfully"
+echo "Service status logged successfully"
